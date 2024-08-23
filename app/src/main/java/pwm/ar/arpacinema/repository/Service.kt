218679ca15e00c3ab6605dda8ac6a039cb822f02
Aurora140818 <159395040+Aurora140818@users.noreply.github.com>
2024-08-23 03:00:34 +0200

    Bottone per ricarica

--- app/src/main/java/pwm/ar/arpacinema/repository/Service.kt
+++ app/src/main/java/pwm/ar/arpacinema/repository/Service.kt
@@ -7,6 +7,7 @@ import retrofit2.http.DELETE
 
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface Service {
 
@@ -27,7 +28,6 @@ interface Service {
 
     @POST("pwm/update_user")
     suspend fun updateUser(@Body updateUserRequest: UpdateUserRequest): Response<GenericResponse>
-
 
 
     // ====================================================================================
