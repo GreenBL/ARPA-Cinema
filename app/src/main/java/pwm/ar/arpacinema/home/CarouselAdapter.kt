efa5d6200fba79974a4b94f881a74fa4ca4acf79
GreenBL <Antonino117@hotmail.it>
2024-09-05 02:35:28 +0200

    lllllllllllllllllllllllllll

--- app/src/main/java/pwm/ar/arpacinema/home/CarouselAdapter.kt
+++ app/src/main/java/pwm/ar/arpacinema/home/CarouselAdapter.kt
@@ -11,16 +11,20 @@ import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.runBlocking
 import pwm.ar.arpacinema.R
+import pwm.ar.arpacinema.databinding.CarouselItemBinding
 
-// TODO: THIS IS AN OLD IMPLEMENTATION OF MINE FROM ANOTHER PROJECT, FIX AND REFACTOR BEFORE APP DELIVERY
+// TODO: THIS IS AN OLD IMPLEMENTATION OF MINE FROM ANOTHER PROJECT, FIX AND REFACTOR BEFORE APP DELIVERY // done but im too lazy to delete the comment
 
-class CarouselAdapter(private val heroList: List<CarouselItem>): RecyclerView.Adapter<CarouselAdapter.CarouselViewHolder>() {
+class CarouselAdapter(
+    private val heroList: List<CarouselItem>,
+    private val onItemClick: (CarouselItem) -> Unit): RecyclerView.Adapter<CarouselAdapter.CarouselViewHolder>() {
 
     // ViewHolder holds the view refs
 
-    class CarouselViewHolder(carouselItem: View): RecyclerView.ViewHolder(carouselItem) {
-        val image: ImageView = carouselItem.findViewById(R.id.movie_image)
-        val title: TextView = carouselItem.findViewById(R.id.movie_title)
+    inner class CarouselViewHolder(val binding: CarouselItemBinding): RecyclerView.ViewHolder(binding.root) {
+        init {
+            // stuff
+        }
     }
 
     // on creation inflate the layout and return the view holder
@@ -29,8 +33,10 @@ class CarouselAdapter(private val heroList: List<CarouselItem>): RecyclerView.Ad
         parent: ViewGroup,
         viewType: Int
     ): CarouselViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.carousel_item, parent, false)
-        return CarouselViewHolder(view)
+        val binding = CarouselItemBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return CarouselViewHolder(binding)
     }
 
     // bind view holder with data from the list using position as index [this will be done for each item]
@@ -38,16 +44,13 @@ class CarouselAdapter(private val heroList: List<CarouselItem>): RecyclerView.Ad
     override fun onBindViewHolder(holder: CarouselViewHolder, position: Int) {
         val currentHero = heroList[position]
 
+        val binding = holder.binding
+        val imageViuh = binding.movieImage
 
         Glide.with(holder.itemView.context)
             .load(currentHero.movieImageUrl)
             .transition(DrawableTransitionOptions.withCrossFade())
-            .into(holder.image)
-
-        // get image with glide
-
-
-        holder.title.text = currentHero.movieTitle
+            .into(imageViuh)
     }
 
     override fun getItemCount(): Int = heroList.size
